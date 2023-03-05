8644e874b4aa07acd16b034da7795be671fcf2d5
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-03-05 21:55:49 +0500

    fix

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -12,18 +12,3 @@
         <button class="btn btn-success">Save</button>
       </form>
 </x-main>
-
-{{-- <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div> --}}
